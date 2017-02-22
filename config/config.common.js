9b804c6c19bc63e9fe9750e4835141b48dd32779
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2017-02-22 12:58:43 +0200

    hosts updated

--- config/config.common.js
+++ config/config.common.js
@@ -1,9 +1,8 @@
 'use strict';
 const utils = require('./utils');
-const hostInfo = require('../../host.json');
 
 module.exports = exports = Object.assign(
-  utils.require_safe('../../host.json') || { host: 'localhost' }, {
+  utils.require_safe('./host.json') || { host: 'localhost' }, {
   env: null,
   apiTitle: 'E-Health Patient-Doctor consent declaration',
   apiPath: '/api/v1',
